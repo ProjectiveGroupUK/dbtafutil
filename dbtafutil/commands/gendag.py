fb7125e4569d9ecb502421c8f9261d667dc7a852
Nitin Garg <nitin.garg@dtsquared.co.uk>
2022-10-31 11:03:48 +0000

    updated

--- dbtafutil/commands/gendag.py
+++ dbtafutil/commands/gendag.py
@@ -7,8 +7,11 @@ logger = logging.getLogger(Logger.getRootLoggerName())
 class GenDagTask(BaseTask):
     def run(self):
         """Entry point for gendag task."""
-        print('hello')
         logger.debug("Generate DAG process initiated..")
+
+        logger.debug(f'Argument models: {self.args.models}')
+        logger.debug(f'Argument select: {self.args.select}')
+        logger.debug(f'Argument tags: {self.args.tags}')
 
     def interpret_results(self, results):
         return True
